cee28a716cb77d674f42a4a2a017ca6524ecc830
goldenmark <marchioro@me.com>
2018-07-31 17:46:12 +0200

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
+module.exports = {
+  Phrase: Phrase,
+  TranslatedPhrase: TranslatedPhrase
+}
+
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
 }
 
-// Defines a Phrase object.
-function Phrase(content) {
-  this.content = content
-
-  this.processor = function(string) {
-    return string.toLowerCase();
+String.prototype.blank = function() {
+  if (this.search(/\s+/g) === -1){return false}
+  else {return this.search(/\s+/g) === this.search(/^\s+$/g)} ;
 }
 
-  this.processedContent = function processedContent() {
-    return this.processor(this.content);
+Array.prototype.last = function() {
+  return this.slice(-1)[0];
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  this.letters = function letters(string) {
+    return Array.from(string).filter(c => c.match(/[a-z]/i)).join("");
+  }
+
+  this.processor = function(string) {
+    return this.letters(string).toLowerCase();
+  }
+
+  this.processedContent = function processedContent(string) {
+    return this.processor(string);
   }
 
   this.palindrome = function palindrome() {
-    return this.processedContent() === this.processedContent().reverse();
+    return this.processedContent(this.content) === this.processedContent(this.content).reverse();
   }
 }
 
@@ -25,12 +43,9 @@ function TranslatedPhrase(content, translation) {
   this.content = content;
   this.translation = translation;
 
-  this.processedContent = function processedContent() {
-    return this.processor(this.translation);
+  this.palindrome = function palindrome() {
+    return this.processedContent(this.translation) === this.processedContent(this.translation).reverse();
   }
 }
 
 TranslatedPhrase.prototype = new Phrase();
-
-frase = new TranslatedPhrase("recognize", "reconocer");
-console.log(frase.palindrome());
